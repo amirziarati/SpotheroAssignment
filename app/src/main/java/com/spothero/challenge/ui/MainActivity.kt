cd5a5f5622eb6235e77ce2511585f49f9aabcf5a
amir ziarati <towarduscceess@gmail.com>
2022-11-14 19:22:50 -0800

    first flickery working version with StateFlow
    and in middle of using one single state object converted to MutableStateFlow instead of two variables

--- app/src/main/java/com/spothero/challenge/ui/MainActivity.kt
+++ app/src/main/java/com/spothero/challenge/ui/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.spothero.challenge.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
-import com.spothero.challenge.data.model.Address
 import com.spothero.challenge.data.model.Spot
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.lazy.items
@@ -22,32 +20,40 @@ import androidx.compose.runtime.*
 import com.spothero.challenge.data.SpotHeroApi
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mainViewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val spots = remember { mutableStateOf<List<Spot>>(mutableListOf()) }
-            SpotList(spots)
-            callSpotsApi(
-                onSuccess = { it ->
-                    spots.value = it
-                },
-                onFail = { throwable ->
-                    Log.e("FirstFragment", throwable.message ?: "onError")
-                })
+            mainViewModel = MainViewModel(SpotHeroApi(this))
+            MainScreen(mainViewModel.selectedSpot)
         }
-
-
     }
 
     @Composable
-    fun SpotList(spots: MutableState<List<Spot>>) {
+    fun MainScreen(selectedSpotState: MutableStateFlow<Spot?>) {
+        val selectedSpot = selectedSpotState.collectAsState(initial = null).value
+        if (selectedSpot == null) {
+            SpotListScreen(mainViewModel.latestSpots, mainViewModel.selectedSpot)
+        } else {
+            SpotDetailsScreen(selectedSpot)
+        }
+    }
+
+    @Composable
+    fun SpotListScreen(spots: Flow<List<Spot>>, selectedSpotState: MutableStateFlow<Spot?>) {
+        val selectedSpot = selectedSpotState.collectAsState(initial = null).value
+        if (selectedSpot != null)
+            return
+        val items = spots.collectAsState(initial = emptyList()).value
         LazyColumn {
             items(
-                items = spots.value,
+                items = items,
                 itemContent = {
                     SpotItem(it)
                 })
@@ -56,8 +62,49 @@ class MainActivity : AppCompatActivity() {
 
     @Composable
     fun SpotItem(spot: Spot) {
-        Column {
+        Column(modifier = Modifier.clickable {
+            val a = 2
+            mainViewModel.selectedSpot.value = spot
+        }) {
             Row(modifier = Modifier.padding(10.dp)) {
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data("file:/" + spot.facilityPhoto)
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = "Parking Spot Image",
+                    modifier = Modifier
+                        .height(72.dp)
+                        .wrapContentWidth()
+                        .padding(0.dp, 0.dp, 16.dp, 0.dp)
+                )
+
+                Column(
+                    modifier = Modifier.height(72.dp),
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(text = spot.address.street)
+                    Text(text = spot.distance)
+                    Text(text = spot.price.toString())
+                }
+            }
+
+            Divider(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .width(1.dp)
+            )
+        }
+
+    }
+
+    @Composable
+    fun SpotDetailsScreen(spot: Spot) {
+        Column {
+            Row(
+                modifier = Modifier
+                    .padding(10.dp)
+            ) {
                 AsyncImage(
                     model = ImageRequest.Builder(LocalContext.current)
                         .data("file:/" + spot.facilityPhoto)
@@ -113,5 +160,13 @@ class MainActivity : AppCompatActivity() {
                 },
                 onFail
             )
+    }
+
+    override fun onBackPressed() {
+        if (mainViewModel.selectedSpot.value != null) {
+            mainViewModel.selectedSpot.value = null
+        } else {
+            super.onBackPressed()
+        }
     }
 }
---
+++ app/src/main/java/com/spothero/challenge/ui/RefreshStrategy.kt
@@ -0,0 +1,5 @@
+package com.spothero.challenge.ui
+
+import java.util.concurrent.TimeUnit
+
+data class RefreshStrategy(val duration: Long, val timeUnit: TimeUnit)
